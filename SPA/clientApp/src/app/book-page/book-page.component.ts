37b8dd95d920ab229dc1a333898ccd6b5f4b6e59
Luka Stanisic <lukasstanisic@gmail.com>
2020-10-15 01:29:09 +0200

    Book page styling

--- SPA/clientApp/src/app/book-page/book-page.component.ts
+++ SPA/clientApp/src/app/book-page/book-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { BooksService } from '../services/books.service';
 import { Book } from '../shared/models/book';
 
@@ -9,14 +10,34 @@ import { Book } from '../shared/models/book';
   templateUrl: './book-page.component.html',
   styleUrls: ['./book-page.component.scss']
 })
-export class BookPageComponent implements OnInit {
-  book$: Observable<Book>;
+export class BookPageComponent implements OnInit, OnDestroy {
+  book: Book;
+  onDestroy$ = new Subject();
 
   constructor(private bookService: BooksService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     const slug = this.route.snapshot.params['slug'];
     
-    this.book$ = this.bookService.getBookInfo(slug);
+    this.bookService.getBookInfo(slug)
+    .pipe(takeUntil(this.onDestroy$))
+    .subscribe((book) => {
+      this.book = book;
+    });
+  }
+
+  toogleUpvote(book: Book){
+    if(book.upvoted){
+      book.upvoted = false;
+      book.upvotes--;
+    } else {
+      book.upvoted = true;
+      book.upvotes++;
+    }
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 }
